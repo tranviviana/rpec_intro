a9b3904f68e015cd60594e11a29712897046406e
tranviviana <tranvivianaedu@gmail.com>
2025-10-31 11:47:10 -0700

    Add thread safe

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,7 +21,7 @@ class MoviesController < ApplicationController
     # default: render 'new' template
   end
   def search_tmdb
-   # later
+   # part 2
   end
   def create
     @movie = Movie.create!(movie_params)
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
-
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
 describe MoviesController do
   describe 'searching TMDb' do
-    it 'calls the model method that performs TMDb search'
+    it 'calls the model method that performs TMDb search' do
+      get :search_tmdb, {:search_terms => 'hardware'}
+    end
     it 'selects the Search Results template for rendering'
     it 'makes the TMDb search results available to that template' 
   end
