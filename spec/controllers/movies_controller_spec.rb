5014bad83fd724b6f2e693e565a69f68c3256942
tranviviana <tranvivianaedu@gmail.com>
2025-10-31 12:02:11 -0700

    part 3

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -13,15 +13,28 @@ if RUBY_VERSION>='2.6.0'
     puts "Monkeypatch for ActionController::TestResponse no longer needed"
   end
 end
+ 
 describe MoviesController do
   describe 'searching TMDb' do
-    it 'calls the model method that performs TMDb search' do
-      fake_results = [double('movie1'), double('movie2')]
-      expect(Movie).to receive(:find_in_tmdb).with('hardware').
-        and_return(fake_results)
-      get :search_tmdb, {:search_terms => 'hardware'}
+    before :each do
+      @fake_results = [double('movie1'), double('movie2')]
     end
-    it 'selects the Search Results template for rendering'
-    it 'makes the TMDb search results available to that template' 
-  end
+    it 'calls the model method that performs TMDb search' do
+      expect(Movie).to receive(:find_in_tmdb).with('hardware').
+       and_return(@fake_results)
+     get :search_tmdb, {:search_terms => 'hardware'}
+   end
+   describe 'after valid search' do
+     before :each do
+       allow(Movie).to receive(:find_in_tmdb).and_return(@fake_results)
+       get :search_tmdb, {:search_terms => 'hardware'}
+     end
+     it 'selects the Search Results template for rendering' do
+       expect(response).to render_template('search_tmdb')
+     end
+     it 'makes the TMDb search results available to that template' do
+       expect(assigns(:movies)).to eq(@fake_results)
+     end
+   end
+ end
 end
